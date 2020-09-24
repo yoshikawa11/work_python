93c79202a0970bbf39b4bddc830f7972902f31e6
yoshikawa masashi <gunterriote@yahoo.co.jp>
2020-09-24 21:11:47 +0900

    CeateViewの追加、リダイレクト先の設定

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import TodoList, TodoDetail
+from .views import TodoList, TodoDetail, TodoCreate
 
 urlpatterns = [
-    path('list/', TodoList.as_view()),
-    path('detail/<int:pk>', TodoDetail.as_view()),
+    path('list/', TodoList.as_view(), name='list'),
+    path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
+    path('create/', TodoCreate.as_view(), name='create'),
 ]
