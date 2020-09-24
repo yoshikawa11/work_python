5d4954ed3f216ddd73d8da157450ea42be6175c1
yoshikawa masashi <gunterriote@yahoo.co.jp>
2020-09-24 21:42:34 +0900

    DeleteViewの追加、削除画面の作成

--- todoproject/todo/urls.py
+++ todoproject/todo/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import TodoList, TodoDetail, TodoCreate
+from .views import TodoList, TodoDetail, TodoCreate, TodoDelete
 
 urlpatterns = [
     path('list/', TodoList.as_view(), name='list'),
     path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
     path('create/', TodoCreate.as_view(), name='create'),
+    path('delete/<int:pk>', TodoDelete.as_view(), name='delete'),
 ]
