511e908f1bfeddf668650ca322fb899b4430d553
yoshikawa masashi <gunterriote@yahoo.co.jp>
2020-10-06 21:53:33 +0900

    signupviewの作り込み

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def signupfunc(request):
     if request.method == 'POST':
-        print('this is post method')
-    else:
-        print('this is get method')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = User.objects.create_user(username, '', password)
+        return render(request, 'signup.html', {'some':100})
+
     return render(request, 'signup.html', {'some':100})
