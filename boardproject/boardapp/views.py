9567e658d912e958d19b97ed8ae9d8acf6076105
yoshikawa masashi <gunterriote@yahoo.co.jp>
2020-10-06 21:13:10 +0900

    ユーザー登録画面のレイアウトを整える

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def signupfunc(request):
+    if request.method == 'POST':
+        print('this is post method')
+    else:
+        print('this is get method')
     return render(request, 'signup.html', {'some':100})
