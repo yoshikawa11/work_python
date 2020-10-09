550a1db195d2bff63c7ab73c710835b27d8aa8e8
yoshikawa masashi <gunterriote@yahoo.co.jp>
2020-10-09 20:49:54 +0900

    ログイン処理の追加 認証されない場合はログインページに遷移

--- boardproject/boardapp/views.py
+++ boardproject/boardapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -14,3 +15,16 @@ def signupfunc(request):
             user = User.objects.create_user(username2, '', password2)
             return render(request, 'signup.html')
     return render(request, 'signup.html', {'some':100})
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login') 
+    return render(request, 'login.html')
