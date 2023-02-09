2544838e23a49428968aebc9192626f33558dac9
yoshikawa masashi <gunterriote@yahoo.co.jp>
2023-02-09 22:29:44 +0900

    commit

--- helloworld/Docker/data/django/helloworldproject/helloworldproject/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-
-def helloworldfunction(request):
-    returnobject = HttpResponse('hello world')
-    return returnobject
-
-class HelloWorldView(TemplateView):
-    template_name = 'hello.html'
